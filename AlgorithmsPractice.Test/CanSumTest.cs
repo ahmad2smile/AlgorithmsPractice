d43af9e2b0e4ff063bd9a8d8657c830cca07ba33
ahmad <ahmad2smile@protonmail.com>
2021-12-05 20:33:55 +0100

    feat: Added Can Sum Resursive

---
+++ AlgorithmsPractice.Test/CanSumTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlgorithmsPractice.Test
+{
+    public class CanSumTest
+    {
+        private CanSum _canSum;
+
+        [SetUp]
+        public void Setup()
+        {
+            _canSum = new CanSum();
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void CanSum_Recursive_True(int targetSum)
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            Assert.IsTrue(_canSum.Rescursive(targetSum, numbers));
+        }
+
+        [Test]
+        [TestCase(300)]
+        [TestCase(900)]
+        public void CanSum_Resursive_False(int targetSum)
+        {
+            var numbers = new List<int> { 7, 14 };
+
+            Assert.IsFalse(_canSum.Rescursive(targetSum, numbers));
+        }
+    }
+}
---
+++ AlgorithmsPractice/CanSum.cs
@@ -0,0 +1,57 @@
+﻿
+namespace AlgorithmsPractice
+{
+    public class CanSum
+    {
+        private readonly Dictionary<int, bool> _cacheCanSum = new();
+
+        // Can we find a sum from given numbers equal to targetSum
+
+        // Can DO: Add Numbers multiple times
+        // Can't DO: ---
+        // Conditon: All Numbers are non-negative
+
+        // Another way to say: Subtract numbers from targetSum so remainder == 0
+
+        // 1. a,b,c  => targetSum -a -b -c == 0
+        // 2. targetSum -a -a -a = 0
+        // 2. targetSum -a -b -b = 0
+        // 2. targetSum -a -b -b = 0
+        // 2. targetSum -b -b -b = 0
+
+        // List: {1,2,3}
+        //                      6
+        //       5              4                   3
+        //  4    3   2   |  3   2   1   |   2        1        0
+        // 321  210  100 | 210  100 000 | 100       000      000
+        public bool Rescursive(int targetSum, List<int> numbers)
+        {
+            if (targetSum == 0)
+            {
+                return true;
+            }
+
+            if (targetSum < 0)
+            {
+                return false;
+            }
+
+            if (_cacheCanSum.TryGetValue(targetSum, out bool result))
+            {
+                return result;
+            }
+
+            foreach (var item in numbers)
+            {
+                result = Rescursive(targetSum - item, numbers);
+
+                if (result || item == 0)
+                {
+                    break;
+                }
+            }
+
+            return _cacheCanSum[targetSum] = result;
+        }
+    }
+}
