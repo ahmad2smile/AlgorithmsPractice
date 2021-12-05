303b648f92bd0bad0bc0550a20ae3b44d214cb11
ahmad <ahmad2smile@protonmail.com>
2021-12-05 20:29:44 +0100

    feat: Added PathFinder Rescursive

---
+++ AlgorithmsPractice.Test/PathFinderTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace AlgorithmsPractice.Test
+{
+    public class PathFinderTest
+    {
+        private PathFinder _pathFinder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _pathFinder = new PathFinder();
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 0, 0)]
+        [TestCase(0, 1, 0)]
+        [TestCase(1, 1, 1)]
+        public void PathFinder_Rescursive_Default(int col, int row, int expectedWays)
+        {
+            Assert.AreEqual(_pathFinder.Rescursive(col, row), expectedWays);
+        }
+
+        [Test]
+        [TestCase(2UL, 3UL, 3UL)]
+        [TestCase(3UL, 2UL, 3UL)]
+        [TestCase(3UL, 3UL, 6UL)]
+        [TestCase(18UL, 18UL, 2333606220UL)]
+        public void PathFinder_Rescursive_Way(ulong col, ulong row, ulong expectedWays)
+        {
+            Assert.AreEqual(_pathFinder.Rescursive((int)col, (int)row), expectedWays);
+        }
+    }
+}
---
+++ AlgorithmsPractice/PathFinder.cs
@@ -0,0 +1,38 @@
+﻿namespace AlgorithmsPractice
+{
+    public class PathFinder
+    {
+        private readonly Dictionary<string, ulong> _cache = new();
+
+        // How many different ways to travel to n,n point (bottom right)
+        // Can't go: Up, Left
+        // Only can go: Right, Down
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+
+        // Down: row - 1
+        // Right: col - 1
+
+        //                      3,3
+        //            3,2                 2,3
+        //             2,2,        1,3
+        //               2,1 1,2      0,3 1,2
+        //                2,0  1,1    2,0  1,1        0,1 1,1
+
+        public ulong Rescursive(int col, int row)
+        {
+            if (col == 1 && row == 1) return 1;
+            if (col == 0 || row == 0) return 0;
+
+            var identifer = $"{col}-{row}";
+
+            if (_cache.TryGetValue(identifer, out ulong cachedVal))
+            {
+                return cachedVal;
+            }
+
+            return _cache[identifer] = Rescursive(col, row - 1) + Rescursive(col - 1, row);
+        }
+    }
+}
