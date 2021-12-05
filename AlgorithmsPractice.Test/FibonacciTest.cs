dc2c47e84a090be7e185d4d617907923cefe14d0
ahmad <ahmad2smile@protonmail.com>
2021-12-05 20:16:34 +0100

    feat: Added Fibonacci Recursive

---
+++ AlgorithmsPractice.Test/FibonacciTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace AlgorithmsPractice.Test
+{
+    public class FibonacciTest
+    {
+        private Fibonacci _fibonacci;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fibonacci = new Fibonacci();
+        }
+
+        [Test]
+        [TestCase(0UL, 0UL)]
+        [TestCase(1UL, 1UL)]
+        public void Fib_Rescursive_Default(ulong nth, ulong expected)
+        {
+            Assert.AreEqual(_fibonacci.Recursive(nth), expected);
+        }
+
+        [Test]
+        [TestCase(2UL, 1UL)]
+        [TestCase(3UL, 2UL)]
+        [TestCase(8UL, 21UL)]
+        [TestCase(9UL, 34UL)]
+        [TestCase(50UL, 12586269025UL)]
+        public void Fib_Rescursive_Of_Nath(ulong nth, ulong expected)
+        {
+            Assert.AreEqual(_fibonacci.Recursive(nth), expected);
+        }
+    }
+}
---
+++ AlgorithmsPractice/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmsPractice
+{
+    public class Fibonacci
+    {
+        private readonly Dictionary<ulong, ulong> _cache = new();
+
+        public ulong Recursive(ulong nth)
+        {
+            if (_cache.TryGetValue(nth, out ulong result))
+            {
+                return result;
+            }
+
+            if(nth < 2) return nth;
+
+            return _cache[nth] = Recursive(nth - 1) + Recursive(nth - 2);
+        }
+    }
+}
