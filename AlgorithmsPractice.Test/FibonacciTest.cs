ef6ef93858a6b59438ce2564c66be0608850cd4a
ahmad <ahmad2smile@protonmail.com>
2021-12-05 20:25:22 +0100

    feat: Added Fabonacci Tabulated

--- AlgorithmsPractice.Test/FibonacciTest.cs
+++ AlgorithmsPractice.Test/FibonacciTest.cs
@@ -30,5 +30,24 @@ namespace AlgorithmsPractice.Test
         {
             Assert.AreEqual(_fibonacci.Recursive(nth), expected);
         }
+
+        [Test]
+        [TestCase(0UL, 0UL)]
+        [TestCase(1UL, 1UL)]
+        public void Fib_Tabulated_Default(ulong nth, ulong expected)
+        {
+            Assert.AreEqual(_fibonacci.Tabulated(nth), expected);
+        }
+
+        [Test]
+        [TestCase(2UL, 1UL)]
+        [TestCase(3UL, 2UL)]
+        [TestCase(8UL, 21UL)]
+        [TestCase(9UL, 34UL)]
+        [TestCase(50UL, 12586269025UL)]
+        public void Fib_Tabulated_Of_Nath(ulong nth, ulong expected)
+        {
+            Assert.AreEqual(_fibonacci.Tabulated(nth), expected);
+        }
     }
 }
--- AlgorithmsPractice/Fibonacci.cs
+++ AlgorithmsPractice/Fibonacci.cs
@@ -15,5 +15,26 @@
 
             return _cache[nth] = Recursive(nth - 1) + Recursive(nth - 2);
         }
+
+        public double Tabulated(ulong nth)
+        {
+            if (nth < 2) return nth;
+
+            var result = 0UL;
+            // NOTE: With Tabulated method you usually save results in a list
+            // but since we are only tracking last 2 results, we can just keep those
+            var prev1 = 0UL;
+            var prev2 = 1UL;
+
+            for (var i = 2UL; i <= nth; i++)
+            {
+                result = prev1 + prev2;
+
+                prev1 = prev2;
+                prev2 = result;
+            }
+
+            return result;
+        }
     }
 }
