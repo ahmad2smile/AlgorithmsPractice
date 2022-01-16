6d317cf6514c7cbe83430ee2e369946f5c6fba02
ahmad <ahmad2smile@protonmail.com>
2022-01-16 18:16:10 +0100

    feat: Added Reversing Linked List

---
+++ AlgorithmsPractice.Test/ReverseLinkedListTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace AlgorithmsPractice.Test;
+
+public class ReverseLinkedListTest
+{
+    private ReverseLinkedList _reeLinkedList;
+
+    [SetUp]
+    public void Setup()
+    {
+        _reeLinkedList = new ReverseLinkedList();
+    }
+
+    [Test]
+    public void Reverses_LinkedList()
+    {
+        var list = new LinkedList<int>();
+        list.AddLast(new LinkedListNode<int>(1));
+        list.AddLast(new LinkedListNode<int>(2));
+        list.AddLast(new LinkedListNode<int>(3));
+        list.AddLast(new LinkedListNode<int>(4));
+        list.AddLast(new LinkedListNode<int>(5));
+
+        var result = _reeLinkedList.SwapValues(list);
+
+        Assert.AreEqual(5, result.First?.Value);
+        Assert.AreEqual(4, result.First?.Next?.Value);
+        Assert.AreEqual(3, result.First?.Next?.Next?.Value);
+        Assert.AreEqual(2, result.First?.Next?.Next?.Next?.Value);
+        Assert.AreEqual(1, result.First?.Next?.Next?.Next?.Next?.Value);
+    }
+
+    [Test]
+    public void Reverses_LinkedList_With_SwapNodes()
+    {
+        var list = new MutableLinkedList<int>
+        {
+            Value = 1
+        };
+
+        list.Head.Add(2).Add(3).Add(4).Add(5);
+
+        var result = _reeLinkedList.SwapNodes(list);
+
+        Assert.AreEqual(5, result?.Value);
+        Assert.AreEqual(4, result?.Next?.Value);
+        Assert.AreEqual(3, result?.Next?.Next?.Value);
+        Assert.AreEqual(2, result?.Next?.Next?.Next?.Value);
+        Assert.AreEqual(1, result?.Next?.Next?.Next?.Next?.Value);
+    }
+
+    [Test]
+    public void Reverses_LinkedList_With_SwapNodes_Recursive()
+    {
+        var list = new MutableLinkedList<int>
+        {
+            Value = 1
+        };
+
+        list.Head.Add(2).Add(3).Add(4).Add(5);
+
+        var result = _reeLinkedList.SwapNodesRecursive(list.Head);
+
+        Assert.AreEqual(5, result?.Value);
+        Assert.AreEqual(4, result?.Next?.Value);
+        Assert.AreEqual(3, result?.Next?.Next?.Value);
+        Assert.AreEqual(2, result?.Next?.Next?.Next?.Value);
+        Assert.AreEqual(1, result?.Next?.Next?.Next?.Next?.Value);
+    }
+}
---
+++ AlgorithmsPractice/ReverseLinkedList.cs
@@ -0,0 +1,127 @@
+﻿namespace AlgorithmsPractice;
+
+public class MutableLinkedList<T>
+{
+    public MutableLinkedList()
+    {
+        Head = this;
+    }
+
+    public MutableLinkedList<T>? Next { get; set; }
+    public MutableLinkedList<T> Head { get; }
+    public T? Value { get; set; }
+
+    public MutableLinkedList<T> Add(T value)
+    {
+        Next = new MutableLinkedList<T>
+        {
+            Value = value
+        };
+
+        return Next;
+    }
+}
+
+public class ReverseLinkedList
+{
+    public MutableLinkedList<int>? SwapNodesRecursive(
+        MutableLinkedList<int> current,
+        MutableLinkedList<int>? prev = null
+    )
+    {
+        var nexNode = current.Next;
+        current.Next = prev;
+
+        return nexNode is null ? current : SwapNodesRecursive(nexNode, current);
+    }
+
+    public MutableLinkedList<int>? SwapNodes(MutableLinkedList<int> list)
+    {
+        MutableLinkedList<int>? prevNode = null;
+        var currentNode = list.Head;
+
+        // null 1 -    2 -    3      4
+        //      prev   c    next
+
+        while (currentNode is not null)
+        {
+            var nextNode = currentNode.Next;
+
+            // NOTE: Adjust pointer of current node to point to prev node
+            currentNode.Next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return prevNode;
+    }
+
+    public LinkedList<int> SwapValues(LinkedList<int> list)
+    {
+        if (list.First is null || list.Last is null)
+        {
+            return list;
+        }
+
+        SwapValues(list.First, 0, GetListLength(list));
+
+        return list;
+    }
+
+    private int GetListLength(LinkedList<int> list)
+    {
+        var length = 0;
+        var currentNode = list.First;
+
+        while (currentNode is not null)
+        {
+            length++;
+            currentNode = currentNode.Next;
+        }
+
+        return length;
+    }
+
+    private LinkedListNode<int>? GetNodeAtLastIndex(
+        LinkedListNode<int> node,
+        int startingIndex,
+        int index,
+        int listLength
+    )
+    {
+        var currentIndex = startingIndex;
+        var indexToFind = listLength - 1 - startingIndex;
+
+        var currentNode = node;
+
+        while (currentIndex != indexToFind && currentNode is not null)
+        {
+            currentNode = currentNode.Next;
+            currentIndex++;
+        }
+
+        return currentNode;
+    }
+
+    // 2, 3, 4, 5, 1
+
+    private void SwapValues(LinkedListNode<int> node, int currentIndex, int listLength)
+    {
+        // NOTE: Last node get is 1 index based as -0 is not recognized to get last index
+        var lastNode = GetNodeAtLastIndex(node, currentIndex, currentIndex, listLength);
+
+        if (lastNode is null || currentIndex >= listLength / 2)
+        {
+            return;
+        }
+
+        (node.Value, lastNode.Value) = (lastNode.Value, node.Value);
+
+        if (node.Next is null)
+        {
+            return;
+        }
+
+        SwapValues(node.Next, currentIndex + 1, listLength);
+    }
+}
