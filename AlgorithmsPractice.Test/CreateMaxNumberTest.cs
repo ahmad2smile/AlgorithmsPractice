e57eb95b97afdb201e68d2e89e8911c4e75027a3
ahmad <ahmad2smile@protonmail.com>
2022-01-23 23:19:41 +0100

    feat: Added Create Max number with adder

---
+++ AlgorithmsPractice.Test/CreateMaxNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace AlgorithmsPractice.Test;
+
+public class CreateMaxNumberTest
+{
+    private CreateMaxNumber _createMaxNumber;
+
+    [SetUp]
+    public void Setup()
+    {
+        _createMaxNumber = new CreateMaxNumber();
+    }
+
+    [Test]
+    public void CanReturn_LargeNumber()
+    {
+        Assert.AreEqual(51234, _createMaxNumber.Iterative(1234, 5));
+        Assert.AreEqual(76543, _createMaxNumber.Iterative(7643, 5));
+        Assert.AreEqual(50, _createMaxNumber.Iterative(0, 5));
+        Assert.AreEqual(-5661, _createMaxNumber.Iterative(-661, 5));
+    }
+}
---
+++ AlgorithmsPractice/CreateMaxNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmsPractice;
+
+public class CreateMaxNumber
+{
+    public int Iterative(int number, int adder)
+    {
+        var isNegative = number < 0;
+
+        var numberStr = number.ToString();
+
+        if (isNegative)
+        {
+            numberStr = numberStr[1..];
+        }
+
+        for (var i = 0; i < numberStr.Length; i++)
+        {
+            var currentDigit = int.Parse(numberStr[i].ToString());
+
+            if (isNegative ? currentDigit < adder : currentDigit > adder)
+            {
+                continue;
+            }
+
+            return (isNegative ? -1 : 1) * int.Parse(numberStr[..i] + adder + numberStr[i..]);
+        }
+
+        return number;
+    }
+}
