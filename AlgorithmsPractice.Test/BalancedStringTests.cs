bf21c62ed273d088e2e502db64bb7ac2360338da
ahmad <ahmad2smile@protonmail.com>
2022-01-23 23:49:21 +0100

    feat: Check Balanced String

---
+++ AlgorithmsPractice.Test/BalancedStringTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace AlgorithmsPractice.Test;
+
+public class BalancedStringTests
+{
+    private BalancedString _balancedString;
+
+    [SetUp]
+    public void Setup()
+    {
+        _balancedString = new BalancedString();
+    }
+
+
+    [Test]
+    public void BalancedString_Return_BalancedStr()
+    {
+        Assert.AreEqual(_balancedString.Iterative("aBAbz"), "aBAb");
+        Assert.AreEqual(_balancedString.Iterative("azABaabza"), "ABaab");
+        Assert.AreEqual(_balancedString.Iterative("AcZCbaBz"), "AcZCbaBz");
+        Assert.AreEqual(_balancedString.Iterative("TacoCat"), "");
+    }
+}
---
+++ AlgorithmsPractice/BalancedString.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgorithmsPractice;
+
+public class BalancedString
+{
+    // 1. Check all uppercase letters have lowercase variants
+    // 2. If not, return a substr that does
+    public string Iterative(string input)
+    {
+        var isBalancedStr = true;
+        var balancedSubStr = "";
+        var listOfSubStrings = new List<string>();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var currentChar = input[i];
+
+            if (!input.Contains(currentChar.ToString().ToUpper()))
+            {
+                isBalancedStr = false;
+
+                listOfSubStrings.Add(balancedSubStr);
+
+                balancedSubStr = "";
+            }
+            else
+            {
+                balancedSubStr += currentChar;
+            }
+        }
+
+        if (isBalancedStr)
+        {
+            return input;
+        }
+
+        var result = listOfSubStrings.Aggregate((a, b) => a.Length > b.Length ? a : b);
+
+        return result.Length < 2 ? "" : result;
+    }
+}
